023674d89c17b45e6d3a90ad697ea838004e14cc
HughBGrant <112141259+HughBGrant@users.noreply.github.com>
2026-07-31 16:26:49 +0900

    Update ordered_set_main.c

--- 06_ordered_set/ordered_set_main.c
+++ 06_ordered_set/ordered_set_main.c
@@ -10,22 +10,22 @@ int main(void)
     bst_insert(bst, 20);
     bst_insert(bst, 40);
 
-    printf("중위 순회: ");
+    printf("inorder: ");
     bst_node_inorder(bst->root);
     printf("\n");
 
     bst_node *node = bst_search(bst, 40);
 
     if (node != NULL) {
-        printf("검색 결과: %d\n", node->data);
+        printf("result: %d\n", node->data);
     }
 
-    printf("최솟값: %d\n",
+    printf("min: %d\n",
            bst_node_get_min(bst->root)->data);
 
     bst->root = bst_node_remove(bst->root, 30);
 
-    printf("30 삭제 후: ");
+    printf("after removing 30: ");
     bst_node_inorder(bst->root);
     printf("\n");
 
